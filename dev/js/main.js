0a69774ee9f02c84e24e8e6371b1555f89b6b3da
Augusto Mota Pinheiro <augustomp55@gmail.com>
2021-09-13 22:05:14 -0400

    Added AR Cité, changed CV pdf to one with working links, removed dream anthem project and made lightboxes easier to close!

--- dev/js/main.js
+++ dev/js/main.js
@@ -5,7 +5,7 @@ require('jquery-mousewheel')($);
 $(function loadCompleted() {
     $('#burger').on("click", () => requestAnimationFrame(openMobileMenu));
 
-    $('#img-modal').on("click", (e) => closeModalImage(e));
+    $('#img-modal').on("click", () => closeModalImage());
     $('[id=img-grid]').each((i, obj) => $(obj).on("mousewheel", (e, d) => scrollImgGrid(e)));
     $('[id=img-child]').each((i, obj) => $(obj).on("click", (e) => openModalImage(e)));
 
@@ -15,7 +15,7 @@ $(function loadCompleted() {
 
     updateHeaderBg();
     activeUnderline();
-})
+});
 
 function openMobileMenu() {
     $('#mobile-menu').addClass('h-screen');
@@ -31,21 +31,23 @@ function scrollImgGrid(e) {
     e.preventDefault();
 }
 
+// Opens the image in a lightbox
 function openModalImage(e) {
     var modal = $('#img-modal');
     modal.children().first().attr('src', $(e.target).attr('src'));
     modal.children().first().attr('alt', $(e.target).attr('alt'));
+    modal.children("#img-caption").html($(e.target).attr('alt'));
     disableBodyScroll(modal);
     modal.removeClass('pointer-events-none opacity-0');
     modal.addClass('pointer-events-auto opacity-100');
 }
 
-function closeModalImage(e) {
-    if (e.target.id == "img-modal") {
-        clearAllBodyScrollLocks();
-        $(e.target).addClass('pointer-events-none opacity-0');
-        $(e.target).removeClass('pointer-events-auto opacity-100');
-    }
+// Closes the image opened in the lightbox
+function closeModalImage() {
+    clearAllBodyScrollLocks();
+    var modal = $('#img-modal');
+    modal.addClass('pointer-events-none opacity-0');
+    modal.removeClass('pointer-events-auto opacity-100');
 }
 
 function updateHeaderBg() {
@@ -72,5 +74,5 @@ function activeUnderline() {
             $(obj).removeClass('w-full');
             $(obj).addClass('w-0');
         }
-    })
+    });
 }
